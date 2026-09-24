b988ad17c5b239cbf869544c4e7e97424533c7d4
Gonzalo Martin Eseiza <78811870+PipaArg@users.noreply.github.com>
2026-09-24 12:42:05 -0300

    Validaciones Pizza
    Agregué @NotBlank en nombre, descripcion, tamanio, tipoMasa; @NotNull + @DecimalMin("0.01") en precio

--- src/main/java/com/bocado/GestorRestaurante/api/dto/PizzaRequest.java
+++ src/main/java/com/bocado/GestorRestaurante/api/dto/PizzaRequest.java
@@ -8,6 +8,16 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class PizzaRequest extends PlatoRequest {
 
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "La descripción es obligatoria")
+    private String descripcion;
+
+    @NotNull(message = "El precio es obligatorio")
+    @DecimalMin(value = "0.01", message = "El precio debe ser mayor a cero")
+    private BigDecimal precio;
+
     @NotBlank(message = "El tamaño es obligatorio")
     private String tamanio;
 
